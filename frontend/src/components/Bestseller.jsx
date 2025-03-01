c4b992b7c23d757c6fd8b9bc76833f44067d8f82
sumit v singh <sumitvikramsingh36@gmail.com>
2025-03-01 22:44:03 +0530

    commit

--- frontend/src/components/Bestseller.jsx
+++ frontend/src/components/Bestseller.jsx
@@ -22,7 +22,7 @@ function Bestseller() {
       </div>
     <div className="mt-2">
   
-    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {bestSellers.map((product) => (
           <ProductItem key={product._id} product={product} />
         ))}
