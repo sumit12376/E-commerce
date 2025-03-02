49d582950a10be52fac70d3509b599527381d6e0
sumit v singh <sumitvikramsingh36@gmail.com>
2025-03-02 19:27:06 +0530

    commit

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/Shopcontext";
 import { Link } from "react-router-dom";
 
 function ProductItem({ product }) {
   const { currency } = useContext(ShopContext);
 
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []);
+
   return (
     <Link to={`/product/${product._id}`} className="block w-full sm:w-64">
       <div className="p-3 sm:p-8 shadow-lg rounded-md hover:shadow-xl transition flex flex-col items-center">
-
-        {/* Container with increased size */}
         <div className="w-48 h-48 sm:w-56 sm:h-56 bg-white rounded-md overflow-hidden flex justify-center items-center">
           <img
             src={product.image?.[0]}
@@ -18,7 +20,7 @@ function ProductItem({ product }) {
           />
         </div>
 
-        <h3 className="text-xs sm:text lg:text-lg font-semibold mt-1 sm:mt-2 text-center ">
+        <h3 className="text-xs sm:text-lg font-semibold mt-1 sm:mt-2 text-center">
           {product.name.length > 15 ? product.name.slice(0, 15) + "..." : product.name}
         </h3>
 
